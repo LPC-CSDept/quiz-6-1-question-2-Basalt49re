8c598dce1bc2814a937e6bec753ef60a364b3d38
Albert <ayang04@zohomail.com>
2022-10-10 20:12:53 -0700

    getInput and findMin debug

--- q2.cpp
+++ q2.cpp
@@ -19,7 +19,17 @@ int main() {
 	findMin(num1,num2,num3);
 	printResult(num1, num2, num3, min);
 }
-
+void getInput(int &n1, int &n2, int &n3){
+	cout << "give me 3 numbers " << endl;
+	cin >> n1 >> n2 >> n3;
+}
+void findMin(int &n1, int &n2, int&n3){
+	int min = n1;
+	if (n2 < min)
+		min = n2; 
+	if (n3 < min)
+		min = n3;
+}
 void printResult(int n1, int n2, int n3, int min)
 {
 	cout << n1 << " " << n2 << " " << n3 << " " << min << " " << endl;
