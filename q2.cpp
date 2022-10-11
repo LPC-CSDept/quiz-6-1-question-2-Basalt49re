acda50434dd711d16f0973ef88e5437c2366cac5
Albert <ayang04@zohomail.com>
2022-10-10 20:10:12 -0700

    main done

--- q2.cpp
+++ q2.cpp
@@ -12,6 +12,14 @@ void printResult(int &n1, int &n2, int &n3);
 
 // ******************************
 // this function will help you pass the test. When you print the result with this function, you will get the same output format
+
+int main() {
+	int num1, num2, num3, min;
+	getInput(num1,num2,num3);
+	findMin(num1,num2,num3);
+	printResult(num1, num2, num3, min);
+}
+
 void printResult(int n1, int n2, int n3, int min)
 {
 	cout << n1 << " " << n2 << " " << n3 << " " << min << " " << endl;
